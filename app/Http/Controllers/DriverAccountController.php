7a9201974abc53bf287f39c5e7a315d13e358861
root <root@srv1291007.hstgr.cloud>
2026-07-18 22:42:52 +0000

    Initial Laravel foundation

---
+++ app/Http/Controllers/DriverAccountController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DriverAccount;
+use Illuminate\Http\Request;
+
+class DriverAccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DriverAccount $driverAccount)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DriverAccount $driverAccount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, DriverAccount $driverAccount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DriverAccount $driverAccount)
+    {
+        //
+    }
+}
---
+++ ql
@@ -0,0 +1,14 @@
+[0;1;32m●[0m mysql.service - MySQL Community Server
+     Loaded: loaded (]8;;file://srv1291007/usr/lib/systemd/system/mysql.service/usr/lib/systemd/system/mysql.service]8;;; [0;1;32menabled[0m; preset: [0;1;32menabled[0m)
+     Active: [0;1;32mactive (running)[0m since Sat 2026-07-18 21:59:45 UTC; 44s ago
+    Process: 811237 ExecStartPre=/usr/share/mysql/mysql-systemd-start pre (code=exited, status=0/SUCCESS)
+   Main PID: 811246 (mysqld)
+     Status: "[0;1;36mServer is operational[0m"
+      Tasks: 38 (limit: 4652)
+     Memory: 361.3M (peak: 377.8M)
+        CPU: 1.118s
+     CGroup: /system.slice/mysql.service
+             └─[0;38;5;245m811246 /usr/sbin/mysqld[0m
+
+Jul 18 21:59:44 srv1291007 systemd[1]: Starting mysql.service - MySQL Community Server...
+Jul 18 21:59:45 srv1291007 systemd[1]: Started mysql.service - MySQL Community Server.
